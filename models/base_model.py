ee0a72630b490be083d86ab5d460524c10db3f52
SundayMba <sundinoh@gmail.com>
2023-12-06 10:25:28 +0100

    create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@ import uuid
 
 class BaseModel:
     """ Base Model Class """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ instance initializer
 
 
@@ -14,9 +14,23 @@ class BaseModel:
             <created_at>:  datetime - assign with the current datetime
             <updated_at>:  datetime - assign with the current datetime
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs == {}:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for key, value in kwargs.items():
+
+                # Exclude the __class__ attribute
+                if key != '__class__':
+                    attr = key
+
+                    # Convert created_at and updated_at to datetime object
+                    if key == 'created_at' or key == 'updated_at':
+                        value = datetime.fromisoformat(value)
+
+                    # Set the attribute using setattr
+                    setattr(self, attr, value)
 
     def __str__(self):
         """ instance representation in string format """
