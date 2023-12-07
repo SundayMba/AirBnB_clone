4035a508a3b84e634be08619ff75a686bfb5b9b8
SundayMba <sundinoh@gmail.com>
2023-12-07 23:07:23 +0100

    implemented do_show, do_all handlers

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import cmd
 from models.base_model import BaseModel
+from models import storage
 """Console module
 this module defines console program entry point
 """
@@ -8,6 +9,21 @@ this module defines console program entry point
 
 class HBNBCommand(cmd.Cmd):
     """ this class is a subclass for cmd.Cmd class """
+
+    @staticmethod
+    def missing_name():
+        """ print class name is missing message """
+        print("** class name missing **")
+
+    @staticmethod
+    def wrong_class():
+        """ print class doesn't exist message """
+        print("** class doesn't exist **")
+
+    @staticmethod
+    def missing_id():
+        """ print instance id is missing message """
+        print("** instance id missing **")
 
     # Custom prompt
     prompt = "(hbnb) "
@@ -33,13 +49,41 @@ class HBNBCommand(cmd.Cmd):
     def do_create(self, model):
         """Create an instance of BaseModel """
         if model == "":
-            print("** class name missing **")
+            self.missing_name()
         elif model != "BaseModel":
-            print("** class doesn't exist **")
+            self.wrong_class()
         else:
             my_model = BaseModel()
             my_model.save()
             print(my_model.id)
+
+    def do_show(self, args):
+        """ show an instance of BaseModel """
+        args_list = args.split()
+        if len(args_list) == 0:
+            self.missing_name()
+        elif len(args_list) == 1 and args != "BaseModel":
+            self.wrong_class()
+        elif len(args_list) == 1 and args == "BaseModel":
+            self.missing_id()
+        else:
+            my_storage = storage.all()
+            for user_id, user_data  in my_storage.items():
+                if args_list[1] == user_data.id:
+                    print(user_data)
+                    return
+            print("** no instance found **")
+
+    def do_all(self, class_name):
+        """ print all instances """
+        if class_name == "" or class_name == "BaseModel":
+            my_storage = storage.all()
+            for user_id in my_storage.keys():
+                print(my_storage[user_id])
+        else:
+            self.wrong_class()
+
+
 
 
 if __name__ == "__main__":
